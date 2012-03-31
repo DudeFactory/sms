a6c773535344fa85f54c0c1459f010fab6d9162e
Dudaev Denis <prosto.duda@gmail.com>
2012-03-31 13:35:48 +0300

    Пример использования SpsRu

---
+++ SmsApi/SmsRu.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmsRu.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiID = "";
+            var baseSms = new SmsBase(apiID, true);
+            //baseSms.Sms.Send(new string[] { "9686262480" }, "test");
+            var a = baseSms.SmsAccount.GetBalance();
+            a.ToString();
+        }
+    }
+}
